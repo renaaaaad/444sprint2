2403a77c1a146a89becf012ac91c8ae1b6a2fff9
renaaaaad <42126910+renaaaaad@users.noreply.github.com>
2019-03-12 01:14:41 +0300

    v19

--- app/src/main/java/com/example/renad/exchangeit/fragment_request.java
+++ app/src/main/java/com/example/renad/exchangeit/fragment_request.java
@@ -3,16 +3,37 @@ package com.example.renad.exchangeit;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import com.example.renad.exchangeit.Adapter.MyFotoAdapter;
+import com.example.renad.exchangeit.Adapter.loadRequestsAdapter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 
 public class fragment_request extends Fragment {
-
-
+RecyclerView recyclerView ;
+    loadRequestsAdapter  loadRequestsAdapter ;
+    List<user_Requests> user_requests ;
+    TextView textView ;
+    String requests_rec ;
     public fragment_request() {
         // Required empty public constructor
     }
@@ -23,9 +44,77 @@ public class fragment_request extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_fragment_request, container, false);
-    }
+        FirebaseUser user=FirebaseAuth.getInstance().getCurrentUser();
+        String userid=user.getUid();
+
+        View view = inflater.inflate(R.layout.fragment_fragment_request, container, false);
+        recyclerView=(RecyclerView)view.findViewById(R.id.recycleRequest);
+        textView = (TextView)view.findViewById(R.id.textView11);
+        recyclerView.setHasFixedSize(true);
+        LinearLayoutManager linearLayoutManager1= new GridLayoutManager(getContext(),1);
+        recyclerView.setLayoutManager(linearLayoutManager1);
+        user_requests =new ArrayList<>();
+        loadRequestsAdapter=new loadRequestsAdapter(getContext(),user_requests);
+        recyclerView.setAdapter(loadRequestsAdapter);
+myRequests(userid);
 
 
+
+
+
+        return view;
+
+    }// on creat
+
+public void myRequests(final String id ){
+
+    DatabaseReference reference3=   FirebaseDatabase.getInstance().getReference("Users").child(id);
+    reference3.child("Receive_requestsNum").addListenerForSingleValueEvent(new ValueEventListener() {
+        @Override
+        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+
+            if (dataSnapshot.getValue().toString().equals("0")){
+                textView.setVisibility(TextView.VISIBLE);
+                return;
+            }
+
+            DatabaseReference reference4=FirebaseDatabase.getInstance().getReference("Users");
+            reference4.child(id).child("requestsReceive").addListenerForSingleValueEvent(new ValueEventListener(){
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                    user_requests.clear();
+
+                    for(DataSnapshot snapshot : dataSnapshot.getChildren()){
+                        user_Requests user_requests1 =snapshot.getValue(user_Requests.class);
+                        user_requests.add(user_requests1);
+
+                    }
+
+                    Collections.reverse(user_requests);
+                    loadRequestsAdapter.notifyDataSetChanged();
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                }
+            });
+
+
+
+
+        }
+
+        @Override
+        public void onCancelled(@NonNull DatabaseError databaseError) {
+
+        }
+    });
+
+
+//--------------------------------------------------------------------------------------------------
+
+}// the end of the finction
 
 }
